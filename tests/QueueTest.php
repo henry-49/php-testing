def01473f41957e4c0e6f10b3547ab4e684ddf1a
Henry Nwadiogor <henryswebdevelopment@gmail.com>
2026-06-29 16:43:48 +0200

    Add Fixtures: Set up known State of thr Tests Using setUp and tearDown

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -1,45 +1,57 @@
 <?php 
 // composer require --dev phpunit/phpunit
 // alias phpunit='vendor/bin/phpunit'
+// phpunit tests/QueueTest.php --colors
+// phpunit --filter testPushAddsItemToQueue
 
 declare(strict_types=1); 
 
 
 use App\Queue;
 use PHPUnit\Framework\Attributes\Test;
-use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\TestCase;
 
 final class QueueTest extends TestCase
 {
+    // Declare a property to hold the Queue instance
+    private Queue $queue;
     
-    public function testNewQueueIsEmpty(): Queue 
+    // This method is called before each test
+    protected function setUp(): void 
     {
-        $queue = new Queue;
-        $this->assertSame(0, $queue->getSize());
-        return $queue;
+        $this->queue = new Queue;
+    }
+
+    // Clean up after each test if necessary
+    protected function tearDown(): void 
+    {
+        unset($this->queue);
+    }
+    
+    public function testNewQueueIsEmpty(): void  
+    {
+        
+        $this->assertSame(0, $this->queue->getSize());
     }
   
-    #[Depends('testNewQueueIsEmpty')]
-    public function testPushAddsItemToQueue(Queue $queue): Queue 
+    public function testPushAddsItemToQueue(): void
     {
-        $queue->push('item1');
-        $this->assertSame(1, $queue->getSize());
-        return $queue;
+        $this->queue->push('item1');
+        $this->assertSame(1, $this->queue->getSize());
     }
 
     #[Test]
-    #[Depends('testPushAddsItemToQueue')]
-    public function popRemovesAndReturnsLastItem(Queue $queue): void 
+    public function popRemovesAndReturnsLastItem(): void 
     {
-        $this->assertSame('item1', $queue->pop());
-        $this->assertSame(0, $queue->getSize());
+        $this->queue->push('item1');
+        $this->assertSame('item1', $this->queue->pop());
+        $this->assertSame(0, $this->queue->getSize());
     }
 
     public function testPopThrowsExceptionWhenQueueIsEmpty(): void 
     {
         $this->expectException(\UnderflowException::class);
-        $queue = new Queue;
-        $queue->pop();
+        $this->queue = new Queue;
+        $this->queue->pop();
     }
 }
